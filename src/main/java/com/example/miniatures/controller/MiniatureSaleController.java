10d971e39aeadcee62a36f4c12733b201db90fca
Pablyco-Espe <pablycohacker@gmail.com>
2025-11-12 00:58:17 +0100

    (feat)update, find by id, delete methods in service and controller.

--- src/main/java/com/example/miniatures/controller/MiniatureSaleController.java
+++ src/main/java/com/example/miniatures/controller/MiniatureSaleController.java
@@ -3,10 +3,8 @@ package com.example.miniatures.controller;
 import com.example.miniatures.model.MiniatureSale;
 import com.example.miniatures.service.MiniatureSaleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,15 +16,38 @@ public class MiniatureSaleController {
     public MiniatureSaleController(MiniatureSaleService miniatureSaleService) {
         this.miniatureSaleService = miniatureSaleService;
     }
-
+    /*
+     * Method to help you to obtain all sales saved in database.
+     */
     @GetMapping("/Sales")
     public List<MiniatureSale> getSales() {
         return miniatureSaleService.getSales();
     }
 
+    /*
+     * Creates a sale.
+     */
+
     @PostMapping("/CreateSale")
     public MiniatureSale createSale(@RequestBody MiniatureSale miniatureSale) {
-        return miniatureSaleService.createMiniatureSale(miniatureSale); //funcion de service
+        return miniatureSaleService.createMiniatureSale(miniatureSale);
+    }
+
+    /*
+     * Update a specific sale by id.
+     */
+    @PutMapping("/Update/{id}")
+    public ResponseEntity<MiniatureSale> updateSaleById(@RequestBody MiniatureSale miniatureSale, @PathVariable Long id) {
+        return miniatureSaleService.updateMiniatureSale(miniatureSale,id);
+    }
+    /*
+     * Delete a specific sale by id.
+     */
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSaleByID(@PathVariable Long id) {
+        miniatureSaleService.deleteMiniatureSale(id);
+        return ResponseEntity.ok().build();
     }
 
 
--- src/main/java/com/example/miniatures/service/MiniatureSaleService.java
+++ src/main/java/com/example/miniatures/service/MiniatureSaleService.java
@@ -1,10 +1,13 @@
 package com.example.miniatures.service;
 
+import com.example.miniatures.exception.ResourceNotFoundException;
 import com.example.miniatures.model.MiniatureSale;
 import com.example.miniatures.repository.MiniatureSaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -25,4 +28,28 @@ public class MiniatureSaleService {
     public MiniatureSale createMiniatureSale(MiniatureSale miniatureSale) {
         return miniatureSaleRepository.save(miniatureSale);
     }
+
+    public Optional<MiniatureSale> getMiniatureSaleById(Long id) {
+        return miniatureSaleRepository.findById(id);
+    }
+
+    public MiniatureSale updateMiniatureSale(MiniatureSale miniatureSale, Long id) {
+        return miniatureSaleRepository.findById(id)
+                .map(MiniatureSale->{
+                    MiniatureSale.setName(miniatureSale.getName());
+                    MiniatureSale.setPrice(miniatureSale.getPrice());
+                    MiniatureSale.setSaleDate(LocalDate.now());
+                    MiniatureSale.setType(miniatureSale.getType());
+                    MiniatureSale.setScale(miniatureSale.getScale());
+                    return miniatureSaleRepository.save(MiniatureSale);
+                })
+                .orElseThrow( ()-> new ResourceNotFoundException("Miniature with ID " + id + " not found"));
+    }
+
+    public void deleteMiniatureSale(Long id) {
+        MiniatureSale sale = miniatureSaleRepository.findById(id)
+                .orElseThrow( ()-> new ResourceNotFoundException("Miniature with ID " + id + " not found"));
+        miniatureSaleRepository.delete(sale);
+    }
+
 }
