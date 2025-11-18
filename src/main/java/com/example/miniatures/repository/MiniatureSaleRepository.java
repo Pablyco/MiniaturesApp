e1eb9cefbdf6e975ee693283196308d1604f7e53
Pablyco <pablycohacker@gmail.com>
2025-11-18 15:10:22 -0300

    (refactor) migrate from derived query methods to JPA Specifications in MiniatureSaleRepository

--- src/main/java/com/example/miniatures/repository/MiniatureSaleRepository.java
+++ src/main/java/com/example/miniatures/repository/MiniatureSaleRepository.java
@@ -4,21 +4,14 @@ import com.example.miniatures.model.MiniatureSale;
 import com.example.miniatures.model.enums.MiniatureScale;
 import com.example.miniatures.model.enums.MiniatureType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface MiniatureSaleRepository extends JpaRepository<MiniatureSale, Long> {
+public interface MiniatureSaleRepository extends JpaRepository<MiniatureSale, Long> , JpaSpecificationExecutor<MiniatureSale> {
     Optional<MiniatureSale> findTopByClientNameOrderBySaleDateDesc(String clientName);
-    List<MiniatureSale> findByClientName(MiniatureType type);
-    List<MiniatureSale> findByType(MiniatureType type);
-    List<MiniatureSale> findByScale(MiniatureScale scale);
-    List<MiniatureSale> findByTypeAndScale(MiniatureType type, MiniatureScale scale);
-    List<MiniatureSale> findByPriceGreaterThan(BigDecimal price);
-    List<MiniatureSale> findByPriceBetween(BigDecimal minPrice,  BigDecimal maxPrice);
-    List<MiniatureSale> findBySaleDateBetween(LocalDate start, LocalDate end);
     List<MiniatureSale> findTop10ByOrderBySaleDateDesc();
-
 }
