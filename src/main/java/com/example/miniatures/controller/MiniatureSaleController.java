35a0baeb12cba7f49bac05eb9def9bbffa3cc3cb
Pablyco <pablycohacker@gmail.com>
2025-11-18 15:27:36 -0300

    (refactor) combine "/sales" endpoint and implement dynamic filtering using specifications

--- src/main/java/com/example/miniatures/controller/MiniatureSaleController.java
+++ src/main/java/com/example/miniatures/controller/MiniatureSaleController.java
@@ -1,10 +1,14 @@
 package com.example.miniatures.controller;
 
 import com.example.miniatures.model.MiniatureSale;
+import com.example.miniatures.model.enums.MiniatureScale;
+import com.example.miniatures.model.enums.MiniatureType;
 import com.example.miniatures.service.MiniatureSaleService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -16,12 +20,55 @@ public class MiniatureSaleController {
         this.miniatureSaleService = miniatureSaleService;
     }
 
+    // *** Miniature sales find methods ***
+
     /*
-     * Method to help you to obtain all sales saved in database.
+     * Main Method to help you to obtain sales saved in database.
      */
-    @GetMapping("/Sales")
-    public ResponseEntity<List<MiniatureSale>> getSales() {
-        List<MiniatureSale> sales = miniatureSaleService.getSales();
+    @GetMapping("/sales")
+    public ResponseEntity<List<MiniatureSale>> getSales(
+            @RequestParam(required = false) MiniatureType type,
+            @RequestParam(required = false) MiniatureScale scale,
+            @RequestParam(required = false) String clientName,
+            @RequestParam(required = false) BigDecimal minPrice,
+            @RequestParam(required = false) BigDecimal maxPrice,
+            @RequestParam(required = false) BigDecimal priceGt,
+            @RequestParam(required = false) BigDecimal priceLt,
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalDate endDate
+
+    ) {
+        var sales = miniatureSaleService.getSales(
+                type,scale,clientName,
+                minPrice,maxPrice,priceGt,
+                priceLt,startDate, endDate
+        );
+        return ResponseEntity.ok(sales);
+
+    }
+
+    /*
+     * Gets a specific sale by its id.
+     */
+    @GetMapping("/sales/{id}")
+    public ResponseEntity<MiniatureSale> getSaleById(@PathVariable long id) {
+        return ResponseEntity.ok(miniatureSaleService.getSaleById(id));
+    }
+
+    /*
+     * Gets the last sale of a client.
+     */
+    @GetMapping("/sales/client/{clientName}/last")
+    public ResponseEntity<MiniatureSale> getLastSaleByClientName(@RequestParam String clientName) {
+        return ResponseEntity.ok(miniatureSaleService.getLastSaleByClientName(clientName));
+    }
+
+    /*
+     * Gets the last 10 sales
+     */
+    @GetMapping("/sales/latest")
+    public ResponseEntity<List<MiniatureSale>> getLastSales(){
+        List<MiniatureSale> sales = miniatureSaleService.getLastSales();
         return ResponseEntity.ok(sales);
     }
 
