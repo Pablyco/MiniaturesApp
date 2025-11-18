2704ead145be1ab7f8431982e93a77bdedcb51eb
Pablyco <pablycohacker@gmail.com>
2025-11-18 15:12:00 -0300

    (refactor) integrate specifications into MiniatureSaleService and remove redundant repository calls

--- src/main/java/com/example/miniatures/service/MiniatureSaleService.java
+++ src/main/java/com/example/miniatures/service/MiniatureSaleService.java
@@ -2,14 +2,16 @@ package com.example.miniatures.service;
 
 import com.example.miniatures.exception.ResourceNotFoundException;
 import com.example.miniatures.model.MiniatureSale;
+import com.example.miniatures.model.enums.MiniatureScale;
 import com.example.miniatures.model.enums.MiniatureType;
 import com.example.miniatures.repository.MiniatureSaleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import com.example.miniatures.specification.MiniatureSaleSpecifications;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,26 +24,107 @@ public class MiniatureSaleService {
         this.miniatureSaleRepository = miniatureSaleRepository;
     }
 
+
+     // *** Miniature sales find/get methods ***
+
     /*
-    *   Find methods
+     * Main Method to help you to obtain sales saved in database.
      */
-    public List<MiniatureSale> getSales() {
-        return miniatureSaleRepository.findAll();
+    public List<MiniatureSale> getSales(
+            MiniatureType type,
+            MiniatureScale scale,
+            String clientName,
+            BigDecimal minPrice,
+            BigDecimal maxPrice,
+            BigDecimal priceGt,
+            BigDecimal priceLt,
+            LocalDate startDate,
+            LocalDate endDate
+    ){
+        /*
+         * Specification<MiniatureSale> spec = Specification.where(null); Deprecated
+         *
+         * New updated method with .unrestricted();
+         */
+        Specification<MiniatureSale> spec = Specification.unrestricted();
+
+        if(type != null) {
+            spec = spec.and(MiniatureSaleSpecifications.hasType(type));
+        }
+
+        if(scale != null) {
+            spec = spec.and(MiniatureSaleSpecifications.hasScale(scale));
+        }
+
+        if(type != null ||  scale != null) {
+            spec = spec.and(MiniatureSaleSpecifications.hasTypeAndScale(type, scale));
+        }
+
+        if(clientName != null) {
+            spec = spec.and(MiniatureSaleSpecifications.clientNameContains(clientName));
+        }
+
+        if(minPrice != null || maxPrice != null){
+            spec = spec.and(MiniatureSaleSpecifications.priceBetween(minPrice,maxPrice));
+        }
+
+        if(priceGt != null){
+            spec = spec.and(MiniatureSaleSpecifications.priceGreaterThan(priceGt));
+        }
+
+        if(priceLt != null){
+            spec = spec.and(MiniatureSaleSpecifications.priceLessThan(priceLt));
+        }
+
+        if (startDate != null || endDate != null) {
+            spec = spec.and(MiniatureSaleSpecifications.saleDateBetween(startDate,endDate));
+        }
+
+        return miniatureSaleRepository.findAll(spec);
+
     }
 
+    /*
+     * Gets a specific sale by its id.
+     */
     public MiniatureSale getSaleById(long id) {
         return miniatureSaleRepository.findById(id)
-                .orElseThrow( ()-> new ResourceNotFoundException("Miniature with ID " + id + " not found"));
+                .orElseThrow( ()-> new ResourceNotFoundException("Miniature Sale with ID " + id + " not found"));
     }
 
-    public List<MiniatureSale> getSalesByType(MiniatureType type) {
-        List<MiniatureSale> sales = miniatureSaleRepository.findByType(type);
-        if (sales.isEmpty()) {
-            throw new ResourceNotFoundException("Sales with miniature type " + type + " not found");
-        }
+    /*
+     * Gets the last sale of a client.
+     */
+    public MiniatureSale getLastSaleByClientName(String clientName) {
+        return miniatureSaleRepository.findTopByClientNameOrderBySaleDateDesc(clientName)
+                .orElseThrow( () -> new ResourceNotFoundException("Miniature Sale of client:  " + clientName + " not found"));
+    }
+
+    /*
+     *  Gets all the sales.
+     */
+    public List<MiniatureSale> getAllSales() {
+        List<MiniatureSale> sales = miniatureSaleRepository.findAll();
+        throwExceptionIfEmpty(sales, "Sales not found");
         return sales;
     }
 
+    /*
+     * Gets the last 10 sales
+     */
+    public List<MiniatureSale> getLastSales(){
+        List<MiniatureSale> sales = miniatureSaleRepository.findTop10ByOrderBySaleDateDesc();
+        throwExceptionIfEmpty(sales, "Sales not found");
+        return sales;
+    }
+
+
+
+
+
+    /*
+     * Create a new Miniature.
+     */
     public MiniatureSale createMiniatureSale(MiniatureSale miniatureSale) {
         return miniatureSaleRepository.save(miniatureSale);
     }
@@ -60,13 +143,19 @@ public class MiniatureSaleService {
                     MiniatureSale.setScale(miniatureSale.getScale());
                     return miniatureSaleRepository.save(MiniatureSale);
                 })
-                .orElseThrow( ()-> new ResourceNotFoundException("Miniature with ID " + id + " not found"));
+                .orElseThrow( ()-> new ResourceNotFoundException("Miniature sale with ID " + id + " not found"));
     }
 
     public void deleteMiniatureSale(Long id) {
         MiniatureSale sale = miniatureSaleRepository.findById(id)
-                .orElseThrow( ()-> new ResourceNotFoundException("Miniature with ID " + id + " not found"));
+                .orElseThrow( ()-> new ResourceNotFoundException("Miniature sale with ID " + id + " not found"));
         miniatureSaleRepository.delete(sale);
+    }
+
+    private static void throwExceptionIfEmpty(List<MiniatureSale> sales, String clientName) {
+        if (sales.isEmpty()) {
+            throw new ResourceNotFoundException(clientName);
+        }
     }
 
 }
