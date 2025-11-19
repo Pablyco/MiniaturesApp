211816614f668726a515aa52f5cffd2f40dcd4d7
Pablyco <pablycohacker@gmail.com>
2025-11-19 17:19:07 -0300

    (refactor) Unused imports were eliminated in MiniatureSaleRepository

--- src/main/java/com/example/miniatures/repository/MiniatureSaleRepository.java
+++ src/main/java/com/example/miniatures/repository/MiniatureSaleRepository.java
@@ -1,13 +1,11 @@
 package com.example.miniatures.repository;
 
 import com.example.miniatures.model.MiniatureSale;
-import com.example.miniatures.model.enums.MiniatureScale;
-import com.example.miniatures.model.enums.MiniatureType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+
 import java.util.List;
 import java.util.Optional;
 
