9bd2e9a5027843ce0c4fe31af12a98f311659a6b
Pablyco <pablycohacker@gmail.com>
2025-11-20 19:12:57 -0300

    (Feat) created MiniatureSaleCreateDTO and MiniatureSaleResponseDTO to improve data transfer management

---
+++ src/main/java/com/example/miniatures/dto/miniatureClient/MiniatureSaleCreateDTO.java
@@ -0,0 +1,38 @@
+﻿package com.example.miniatures.dto.miniatureClient;
+
+
+import com.example.miniatures.model.enums.MiniatureScale;
+import com.example.miniatures.model.enums.MiniatureType;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MiniatureSaleCreateDTO {
+
+    @NotBlank(message = "Miniature name cant be empty")
+    String name;
+
+    @NotNull(message = "Price is obligatory")
+    @Positive(message = "Price should be greater than zero '0' ")
+    BigDecimal price;
+
+    @NotNull(message = "The sale date is obligatory")
+    @PastOrPresent(message = "The sale date can't be assigned in the future")
+    LocalDate saleDate;
+
+    @NotNull(message = "Miniature type is obligatory")
+    MiniatureType type;
+
+    @NotNull(message = "Miniature scale is obligatory")
+    MiniatureScale scale;
+
+    @NotNull(message = "Client ID is obligatory")
+    @Positive@Positive(message = "Client ID should be greater than zero '0' ")
+    Long clientId;
+}
---
+++ src/main/java/com/example/miniatures/dto/miniatureClient/MiniatureSaleResponseDTO.java
@@ -0,0 +1,29 @@
+﻿package com.example.miniatures.dto.miniatureClient;
+
+import com.example.miniatures.model.enums.MiniatureScale;
+import com.example.miniatures.model.enums.MiniatureType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class MiniatureSaleResponseDTO{
+
+    //sale information
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private LocalDate saleDate;
+    private MiniatureType type;
+    private MiniatureScale scale;
+
+    //Client information
+    private Long clientId;
+    private String clientName;
+}
