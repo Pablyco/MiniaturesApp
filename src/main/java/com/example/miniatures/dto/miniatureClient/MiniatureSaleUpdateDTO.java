f349dfff8a7063626eeb150f7034a59ce1a49ad3
Pablyco <pablycohacker@gmail.com>
2025-11-21 15:29:04 -0300

    (refactor) remove duplicated code and cleanup redundant imports. Now inherited from MiniatureSaleBaseDTO

---
+++ src/main/java/com/example/miniatures/dto/miniatureClient/MiniatureSaleUpdateDTO.java
@@ -0,0 +1,15 @@
+﻿package com.example.miniatures.dto.miniatureClient;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MiniatureSaleUpdateDTO extends MiniatureSaleBaseDTO{
+
+
+}
