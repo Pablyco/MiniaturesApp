8d3f546f907fbf325cc8bc8cb21410e1365902c5
Pablyco <pablycohacker@gmail.com>
2025-11-25 09:58:40 -0300

    (feat)Created a DTO for specification filters.

---
+++ src/main/java/com/example/miniatures/dto/miniatureSale/SalesFilterDTO.java
@@ -0,0 +1,35 @@
+﻿package com.example.miniatures.dto.miniatureSale;
+
+import com.example.miniatures.model.enums.MiniatureScale;
+import com.example.miniatures.model.enums.MiniatureType;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public abstract class SalesFilterDTO {
+
+    MiniatureType type;
+    MiniatureScale scale;
+
+    @Positive(message = "Client ID should be greater than zero '0' ")
+    Long clientId;
+
+    @Positive(message = "Price should be greater than zero '0' ")
+    BigDecimal minPrice;
+
+    @Positive(message = "Price should be greater than zero '0' ")
+    BigDecimal maxPrice;
+
+    LocalDate startDate;
+    LocalDate endDate;
+}
