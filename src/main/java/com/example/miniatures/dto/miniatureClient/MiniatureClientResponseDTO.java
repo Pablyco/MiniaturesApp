dc639c7874dff29d66ff76b59b4bede4143b922f
Pablyco <pablycohacker@gmail.com>
2025-11-25 16:23:52 -0300

    (feat)A DTO for Client Response was created.

---
+++ src/main/java/com/example/miniatures/dto/miniatureClient/MiniatureClientResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.miniatures.dto.miniatureClient;
+
+import com.example.miniatures.dto.miniatureSale.MiniatureSaleResponseDTO;
+
+import java.util.List;
+
+public class MiniatureClientResponseDTO extends MiniatureClientBaseDTO{
+
+    List<MiniatureSaleResponseDTO> sales;
+}
