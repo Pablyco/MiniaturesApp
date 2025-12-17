835f46564f68ce34da004fac81d9a4d3605e9b65
Pablyco <pablycohacker@gmail.com>
2025-12-17 12:22:38 -0300

    (refactor) cleaned up redundant code in UserRole enum

--- src/main/java/com/example/miniatures/model/enums/UserRole.java
+++ src/main/java/com/example/miniatures/model/enums/UserRole.java
@@ -4,13 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum UserRole {
-    ROLE_ADMIN("Admin role, can control everything"),
-    ROLE_CLIENT("Client role, can control some functionalities");
-
-    private final String authority;
-
-    UserRole(String authority) {
-        this.authority = authority;
-    }
-
+    ROLE_ADMIN,
+    ROLE_CLIENT;
 }
