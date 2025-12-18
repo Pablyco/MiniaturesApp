ac0922da9672994cee3593886705b345fedcb151
Pablyco <pablycohacker@gmail.com>
2025-12-18 11:57:07 -0300

    (feat) SaleNotFoundException was created

--- src/main/java/com/example/miniatures/exception/SaleNotFoundException.java
+++ src/main/java/com/example/miniatures/exception/SaleNotFoundException.java
@@ -1,4 +1,12 @@
 package com.example.miniatures.exception;
 
-public class SaleNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+
+public class SaleNotFoundException extends RuntimeException {
+    public SaleNotFoundException(String message) {
+        super(message);
+    }
 }
